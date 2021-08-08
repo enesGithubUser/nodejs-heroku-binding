287cf23c4b2a3fc2b44165f41f22d050029c2cbc
enesgituser <enes_gkaya@hotmail.com>
2021-08-08 08:13:54 +0300

    create table dosyası basitlestirildi.

--- data/migrations/create_table.js
+++ data/migrations/create_table.js
@@ -6,18 +6,6 @@ exports.up = function (knex) {
         table.string("name").notNullable().index();
         table.text("email").notNullable();
         table.datetime("created_at").defaultTo(knex.fn.now());
-      })
-      .createTable("post", (table) => {
-        table.increments();
-        table.string("display_name").notNullable();
-        table.text("body").notNullable();
-        table.datetime("created_at").defaultTo(knex.fn.now());
-        table.integer("user_id").unsigned();
-        table
-          .foreign("user_id")
-          .references("user.id")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
       });
   };
   
